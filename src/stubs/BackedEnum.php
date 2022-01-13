b44c50f264d088d4759d7792c4f381b5f1b7cd97
Marc Bennewitz <m.bennewitz@dcmn.com>
2022-01-13 15:25:11 +0100

    fixed filename to import

--- src/stubs/BackedEnum.php
+++ src/stubs/BackedEnum.php
@@ -3,5 +3,5 @@
 if (PHP_VERSION_ID < 80000) {
     require_once __DIR__ . '/../php74/BackedEnum.php';
 } else {
-    require_once __DIR__ . '/../php80/BackedEnum0.php';
+    require_once __DIR__ . '/../php80/BackedEnum.php';
 }
